c126a66cd58ec927ec83dd01295cb423f45413ca
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2019-09-26 09:44:07 +0200

    Add custom cluster generation script for mempool

--- python/generators/v1/chip_gen.py
+++ python/generators/v1/chip_gen.py
@@ -17,7 +17,7 @@
 
 from generators.v1.comp_gen import *
 import generators.v1.mempool_soc_gen as soc
-import generators.v1.cluster_gen as cluster
+import generators.v1.mempool_cluster_gen as cluster
 import math
 
 def get_config(tp):
