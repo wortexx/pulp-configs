0f48410d88fcea1f127f7937b7297e19ce9f4d21
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2020-01-14 19:51:12 +0100

    Added dummy hwpe model

--- python/generators/v1/cluster_gen.py
+++ python/generators/v1/cluster_gen.py
@@ -28,6 +28,7 @@ def get_config(tp, cluster_id):
   cluster_version   = tp.get_child_int('cluster/version')
   has_cc            = tp.get_child_bool('cluster/has_cc')
   has_hwce          = tp.get('cluster/peripherals/hwce') is not None
+  has_hwacc          = tp.get('cluster/peripherals/hwacc') is not None
   dma_irq_0         = tp.get('cluster/pe/irq').get_dict().index('dma_0')
   dma_irq_1         = tp.get('cluster/pe/irq').get_dict().index('dma_1')
   dma_irq_ext       = tp.get('cluster/pe/irq').get_dict().index('dma_ext')
@@ -44,6 +45,9 @@ def get_config(tp, cluster_id):
 
   if has_hwce:
     hwce_irq         = tp.get('cluster/pe/irq').get_dict().index('acc_0')
+
+  if has_hwacc:
+    hwacc_irq         = tp.get('cluster/pe/irq').get_dict().index('acc_0')
 
   core_conf = js.import_config_from_file("ips/riscv/%s.json" % cluster_core, find=True)
 
@@ -123,6 +127,11 @@ def get_config(tp, cluster_id):
       ("hwce", get_mapping_area(tp.get_child_dict("cluster/peripherals/hwce"), cluster_size, cluster_id, True))
     ]))
 
+  if has_hwacc:
+    periph_ico_mappings.update(OrderedDict([
+      ("hwacc", get_mapping_area(tp.get_child_dict("cluster/peripherals/hwacc"), cluster_size, cluster_id, True))
+    ]))
+
   cluster.periph_ico = Component(properties=OrderedDict([
     ('includes', ["ips/interco/router.json"]),
     ('mappings', periph_ico_mappings)
@@ -187,6 +196,8 @@ def get_config(tp, cluster_id):
 
   l1_interleaver_nb_masters = nb_pe + 4
   if has_hwce:
+    l1_interleaver_nb_masters += 4
+  if has_hwacc:
     l1_interleaver_nb_masters += 4
 
   cluster.l1_ico.interleaver = Component(properties=OrderedDict([
@@ -265,6 +276,11 @@ def get_config(tp, cluster_id):
       ('includes', ["ips/hwce/hwce_v%d.json" % tp.get_child_int('cluster/peripherals/hwce/version')])
     ]))
 
+  if has_hwacc:
+    cluster.hwacc = Component(properties=OrderedDict([
+      ('includes', ["ips/hwacc/hwacc.json"])
+    ]))
+
   cluster.icache_ctrl = Component(properties=OrderedDict([
     ('includes', ["ips/icache_ctrl/icache_ctrl_v2.json"])
   ]))
@@ -324,6 +340,8 @@ def get_config(tp, cluster_id):
   cluster.periph_ico.icache_ctrl = cluster.icache_ctrl.input
   if has_hwce:
     cluster.periph_ico.hwce = cluster.hwce.input
+  if has_hwacc:
+    cluster.periph_ico.hwacc = cluster.hwacc.input
   cluster.periph_ico.event_unit = cluster.event_unit.input
   cluster.periph_ico.cluster_ctrl = cluster.cluster_ctrl.input
   cluster.periph_ico.timer = cluster.timer.input
@@ -333,6 +351,10 @@ def get_config(tp, cluster_id):
   if has_hwce:
     for i in range(0, nb_pe):
       cluster.hwce.irq = cluster.event_unit.new_itf('in_event_%d_pe_%d' % (hwce_irq, i))
+
+  if has_hwacc:
+    for i in range(0, nb_pe):
+      cluster.hwacc.irq = cluster.event_unit.new_itf('in_event_%d_pe_%d' % (hwacc_irq, i))
 
   for i in range(0, nb_pe):
     cluster.periph_ico.set('dbg_unit_%d' % i, cluster.get('pe%d' % i).dbg_unit)
@@ -419,6 +441,11 @@ def get_config(tp, cluster_id):
     for i in range(0, 4):
       cluster.hwce.set('out_%d' % i, cluster.l1_ico.new_itf('hwce_in_%d' % i))
       cluster.l1_ico.set('hwce_in_%d' % i, cluster.l1_ico.interleaver.new_itf('in_%d' % (nb_pe + 4 + i)))
+
+  if has_hwacc:
+    for i in range(0, 4):
+      cluster.hwacc.set('out_%d' % i, cluster.l1_ico.new_itf('hwacc_in_%d' % i))
+      cluster.l1_ico.set('hwacc_in_%d' % i, cluster.l1_ico.interleaver.new_itf('in_%d' % (nb_pe + 4 + i)))
 
   for i in range(0, nb_pe):
     cluster.l1_ico.get('pe%d_ico' % i).dma = cluster.l1_ico.new_itf('dma_%d'%i)
